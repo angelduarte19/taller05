4740e49d50c2f7a00d1d5970e32827b2bd6e75eb
angelduarte19 <109190181+angelduarte19@users.noreply.github.com>
2023-10-27 21:54:41 -0300

    Add files via upload

---
+++ tallerphp05/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arreglos</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h3>Arreglos con php</h3>
+        <?php
+            $nombre[] = "JUAN";
+            $nombre[] = "ROSA";
+            $nombre[] = "JULIO";
+            $nombre[] = "ARACELI";
+            $nombre[] = "SOFIA";
+            //listar
+            for ($indice=0; $indice < count($nombre) ; $indice++){
+                echo "Nombre: " . $nombre[$indice] . "<br>";
+            }
+            
+        ?>
+        <table class="table">
+            <thead>
+                <th>Nombres</th>
+            </thead>
+            <tbody>
+                <?php
+                    for ($i=0; $i < count($nombre) ; $i++) { 
+                    ?> 
+                        <tr><td><?=$nombre[$i];?></td></tr>
+                    <?php
+                    }
+                ?>
+            </tbody>
+
+        </table>
+        <table class="table">
+            <thead>
+                <th>Forma de pagos</th>
+            </thead>
+            <tbody>
+                <?php
+                    $fpagos = array("Efectivo", "Tarjeta", "Transferencia","Paypal");
+                    for ($i=0; $i < count($fpagos) ; $i++) { 
+                    ?> 
+                        <tr><td><?=$fpagos[$i];?></td></tr>
+                    <?php
+                    }
+                ?>
+            </tbody>
+
+        </table>
+    </div>
+</body>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.min.js"></script>
+</html>
